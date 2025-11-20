77e41be22f4271d7dda42100c3e4a71426b51a4a
Lin Htet Shein <127378804+Lawrecesss@users.noreply.github.com>
2025-11-20 09:53:19 +0000

    just nth and smth between

--- include/WebServer.hpp
+++ include/WebServer.hpp
@@ -6,7 +6,7 @@
 /*   By: lshein <lshein@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 07:33:13 by lshein            #+#    #+#             */
-/*   Updated: 2025/11/20 08:57:07 by lshein           ###   ########.fr       */
+/*   Updated: 2025/11/20 09:25:45 by lshein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,12 @@
 #include "proxyPass.h"
 
 #define MAX_EVENTS 10
-
-typedef struct its
-{
-	std::string::iterator it1;
-	std::string::iterator it2;
-} t_its;
-
 class WebServer
 {
 private:
 	std::vector<Server> _servers;
 	std::vector<Socket> _sockets;
 	// std::vector<pollfd> _pollFds;
-	std::string getIndex(std::string path);
 
 public:
 	WebServer();
--- src/classes/Server.cpp
+++ src/classes/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: lshein <lshein@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 07:59:51 by lshein            #+#    #+#             */
-/*   Updated: 2025/11/20 09:07:39 by lshein           ###   ########.fr       */
+/*   Updated: 2025/11/20 09:27:26 by lshein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -237,8 +237,8 @@ void Server::setServerInfo(const std::vector<std::string> &line)
 	if (line.empty())
 		return;
 
-	const std::string &key = line[0];
-	std::map<std::string, AttrHandler>::iterator it = getServerHandlers().find(key);
+	const std::string &directive = line[0];
+	std::map<std::string, AttrHandler>::iterator it = getServerHandlers().find(directive);
 	if (it != getServerHandlers().end())
 	{
 		AttrHandler handler = it->second;
