49adbe8b21a83cbe6d160be31fb36528b156107f
Thant Htet Aung <maung.good@gmail.com>
2025-12-16 19:10:25 +0800

    extra . on include fix

--- src/classes/WebServer.cpp
+++ src/classes/WebServer.cpp
@@ -6,11 +6,11 @@
 /*   By: taung <taung@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 07:51:13 by lshein            #+#    #+#             */
-/*   Updated: 2025/12/15 16:39:19 by taung            ###   ########.fr       */
+/*   Updated: 2025/12/16 19:06:52 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./../../include/WebServer.hpp"
+#include "../../include/WebServer.hpp"
 #include "../../include/Request.hpp"
 #include "../../include/Response.hpp"
 #include <sys/types.h>
